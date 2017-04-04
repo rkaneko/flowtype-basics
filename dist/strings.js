2daa3668769af4939071bd07e1ef003a73c942ef
rkaneko <kanek.o.dev@gmail.com>
2017-04-05 01:48:10 +0900

    `add` a strings example

---
+++ dist/strings.js
@@ -0,0 +1,9 @@
+/*  */
+
+('blahblah');
+('true');
+('');
+const text1 = undefined;  // Error
+const text2 = 1;  // Error
+const text3 = true;  // Error
+const text4 = {};  // Error
---
+++ src/strings.js
@@ -0,0 +1,9 @@
+/* @flow */
+
+('blahblah': string);
+('true': string);
+('': string);
+const text1: string = undefined;  // Error
+const text2: string = 1;  // Error
+const text3: string = true;  // Error
+const text4: string = {};  // Error
