18729d1be4c0f3da96f89d4665bfc74df093e808
rkaneko <kanek.o.dev@gmail.com>
2017-04-05 01:56:58 +0900

    `add` examples to strings examples

--- dist/strings.js
+++ dist/strings.js
@@ -7,3 +7,5 @@ const text1 = undefined;  // Error
 const text2 = 1;  // Error
 const text3 = true;  // Error
 const text4 = {};  // Error
+'' + String(false);
+'' + JSON.stringify({});
--- src/strings.js
+++ src/strings.js
@@ -7,3 +7,5 @@ const text1: string = undefined;  // Error
 const text2: string = 1;  // Error
 const text3: string = true;  // Error
 const text4: string = {};  // Error
+'' + String(false);
+'' + JSON.stringify({});
